22a11b1fa9d43b4d23d15068672ce17cf67223fc
Matthew Poletiek <matthew.poletiek@gmail.com>
2023-07-08 04:58:06 +0000

    donate page work

--- api/polygonscan.php
+++ api/polygonscan.php
@@ -7,7 +7,7 @@ header('Content-Type: application/json; charset=utf-8');
 $return_obj = new stdClass();
 $API_URL = "https://api.polygonscan.com/api";
 
-// Get Balance
+// Get MATIC Balance
 $GET_BALANCE_OPTS = "?module=account&action=balance&address=".$TM_SAFE_ADDRESS."&apikey=".$POLYGONSCAN_API_TOKEN;
 $api_endpoint = $API_URL.$GET_BALANCE_OPTS;
 $curl = curl_init($api_endpoint);
@@ -16,11 +16,24 @@ $resp = curl_exec($curl);
 curl_close($curl);
 $resp_obj = json_decode($resp);
 $resp_balance = $resp_obj->result;
-
 // Format balance
 $matic_balance = (int)$resp_balance / 1000000000000000000;
 $matic_balance = round($matic_balance,4);
 $return_obj -> matic_balance = $matic_balance;
+
+// Get DAI Balance
+$GET_DAI_BALANCE_OPTS = "?module=account&action=tokenbalance&contractaddress=".$DAI_CONTRACT_ADDRESS."&address=".$TM_SAFE_ADDRESS."&tag=latest&apikey=".$POLYGONSCAN_API_TOKEN;
+$get_dai_balance_endpoint = $API_URL.$GET_DAI_BALANCE_OPTS;
+$curl = curl_init($get_dai_balance_endpoint);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
+$resp = curl_exec($curl);
+curl_close($curl);
+$resp_obj = json_decode($resp);
+$resp_balance = $resp_obj->result;
+// Format balance
+$dai_balance = (int)$resp_balance / 1000000000000000000;
+$dai_balance = round($dai_balance,4);
+$return_obj -> dai_balance = $dai_balance;
 
 // Get latest block
 $GET_BLOCK_OPTS = "?module=proxy&action=eth_blockNumber&apikey=".$POLYGONSCAN_API_TOKEN;
@@ -33,7 +46,7 @@ $resp_obj = json_decode($resp);
 $latest_block = hexdec($resp_obj->result);
 $latest_block = (int)$latest_block;
 
-// Get Transactions
+// Get Normal Transactions
 $GET_TRANSACTIONS_OPTS = "?module=account&action=txlist&address=".$TM_SAFE_ADDRESS."&startblock=0&endblock=".$latest_block."&page=1&offset=10&sort=dec&apikey=".$POLYGONSCAN_API_TOKEN;
 $get_transactions_api_endpoint = $API_URL.$GET_TRANSACTIONS_OPTS;
 $curl = curl_init($get_transactions_api_endpoint);
@@ -41,15 +54,31 @@ curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
 $resp = curl_exec($curl);
 curl_close($curl);
 $resp_obj = json_decode($resp);
-
-// Record Transactions
+// Record Normal Transactions
 $return_obj->transactions = [];
-$tx_count = 0;
 foreach ($resp_obj->result as $tx){
     $amount = $tx->value / 1000000000000000000;
     $amount = round($amount,4);
-    array_push($return_obj->transactions, [$tx->from,$amount]);
+    array_push($return_obj->transactions, array('token'=>'matic','from'=>$tx->from,'timestamp'=>$tx->timeStamp,'amount'=>$amount));
 }
+
+// Get DAI Transactions
+$GET_ERC20TX_OPTS = "?module=account&action=tokentx&contractaddress=".$DAI_CONTRACT_ADDRESS."&address=".$TM_SAFE_ADDRESS."&startblock=0&endblock=".$latest_block."&page=1&offset=5&sort=dec&apikey=".$POLYGONSCAN_API_TOKEN;
+$get_daitx_api_endpoint = $API_URL.$GET_ERC20TX_OPTS;
+$curl = curl_init($get_daitx_api_endpoint);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
+$resp = curl_exec($curl);
+curl_close($curl);
+$resp_obj = json_decode($resp);
+// Record DAI Transactions
+foreach ($resp_obj->result as $tx){
+    $amount = $tx->value / 1000000000000000000;
+    $amount = round($amount,4);
+    array_push($return_obj->transactions, array('token'=>'dai','from'=>$tx->from,'timestamp'=>$tx->timeStamp,'amount'=>$amount));
+}
+
+$ret_tx_columns = array_column($return_obj->transactions,'timestamp');
+array_multisort($ret_tx_columns,SORT_DESC,$return_obj->transactions);
 
 $json_return = json_encode($return_obj);
 echo $json_return;
--- js/donate.js
+++ js/donate.js
@@ -5,6 +5,7 @@ var DAIBalance;
 var MATICBalance;
 
 const DAIContractAddress = "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063";
+const TMSafeAddress = "0xddF18000aba4e7EDBa1fc87a089B36051C83Eb27";
 
 // ABI for getting ERC20 Balance
 const minABI = [
@@ -27,6 +28,56 @@ const minABI = [
     type: "function",
   },
 ];
+
+async function sendDonation(){
+  //console.log("Sending Donation: "+document.getElementById('donation-amount').value);
+  document.getElementById('donate-error').innerHTML = "";
+  if(document.getElementById('donation-amount').value > 0){
+    donationAmount = document.getElementById('donation-amount').value;
+    try {
+      await web3.eth.sendTransaction({
+        from:accounts[0],
+        to:TMSafeAddress,
+        value:web3.utils.toWei(donationAmount,"ether"),
+      })
+      .on('error', function(err){
+        console.log('error:');
+        console.log(err);
+      })
+      .on('transactionHash', function(transactionHash){
+        //console.log("txHash:");
+        //console.log(transactionHash);
+        document.getElementById('donate-error').innerHTML = "<small class=\"text-success\"><a href=\"https://polygonscan.com/tx/"+transactionHash+"\" target=\"_blank\" class=\"link-light\">"+transactionHash+"</a> Submitted.";
+        document.getElementById('donation-amount').value = 0;
+        document.getElementById('donate-button').disabled = true;
+      })
+      .on('receipt',function(receipt){
+        //console.log("Receipt:");
+        //console.log(receipt);
+        document.getElementById('donate-error').innerHTML = "<small class=\"text-success\"><a href=\"https://polygonscan.com/tx/"+receipt.transactionHash+"\" target=\"_blank\" class=\"link-light\">"+receipt.transactionHash+"</a> Success!";
+      });
+    }
+    catch(e){
+      console.log(e.message);
+      document.getElementById('donate-error').innerHTML = "<small class=\"text-danger\">"+e.message+"</small>";
+    }
+    
+  }
+}
+
+function donationAmountChange(){
+  if(document.getElementById('select-token').value == 'null'){
+    document.getElementById('donate-button').disabled = true;
+  }
+  else{
+    if(document.getElementById('donation-amount').value > 0){
+      document.getElementById('donate-button').disabled = false;
+    }
+    else{
+      document.getElementById('donate-button').disabled = true;
+    }
+  }
+}
 
 function setMaxDonation() {
   selectVal = document.getElementById('select-token').value;
@@ -58,14 +109,17 @@ function selectToken() {
   if(selectVal == 'matic'){
     matic = Math.round(MATICBalance * 10000) / 10000;
     document.getElementById('max-amount').innerHTML = "Max Amount: "+matic;
+    donationAmountChange();
   }
   else if(selectVal == 'dai') {
     dai = Math.round(DAIBalance * 10000) / 10000;
     document.getElementById('max-amount').innerHTML = "Max Amount: "+dai;
+    donationAmountChange();
   }
   else if(selectVal == 'null') {
     matic = Math.round(MATICBalance * 10000) / 10000;
     document.getElementById('max-amount').innerHTML = "";
+    donationAmountChange();
   }
 }
 
@@ -106,72 +160,109 @@ async function switchNetwork(){
 
 async function connectWallet(){
     document.getElementById("connect-error").innerHTML = '';
-    console.log("Hello");
+    //console.log("Hello");
     didError = false;
     if (typeof window.ethereum !== 'undefined') {
-        // Use the browser injected Ethereum provider
-        web3 = new Web3(window.ethereum);
-        // Request access to the user's MetaMask account.
-        // Note: Even though, you can also get the accounts from `await web3.eth.getAccounts()`,
-        //  you still need to make a call to any MetaMask RPC to cause MetaMask to ask for concent.
-        try {
-            accounts = await window.ethereum.request({
-                method: 'eth_requestAccounts',
-            });
-        }
-        catch(e){
-            //console.log(e.message);
-            document.getElementById("connect-error").innerHTML = e.message;
-            didError = true;
-        }
-        
-        console.log('Accounts requested from MetaMask RPC: ', accounts);
-        didError = await switchNetwork();
+      // Use the browser injected Ethereum provider
+      web3 = new Web3(window.ethereum);
+      // Request access to the user's MetaMask account.
+      // Note: Even though, you can also get the accounts from `await web3.eth.getAccounts()`,
+      //  you still need to make a call to any MetaMask RPC to cause MetaMask to ask for concent.
+      try {
+          accounts = await window.ethereum.request({
+              method: 'eth_requestAccounts',
+          });
+      }
+      catch(e){
+          //console.log(e.message);
+          document.getElementById("connect-error").innerHTML = e.message;
+          didError = true;
+      }
+      
+      //console.log('Accounts requested from MetaMask RPC: ', accounts);
+      didError = await switchNetwork();
 
-        if(!didError){
-            document.getElementById('connect-button-div').style.display = 'none';
-            document.getElementById('donate-form-card').style.display = 'block';
-            window.ethereum.on('chainChanged', (chainId) => {
-                if(chainId !== '0x89'){
-                    window.location.reload();
-                }
-            });
+      if(!didError){
+          document.getElementById('connect-button-div').style.display = 'none';
+          document.getElementById('donate-form-card').style.display = 'block';
+          window.ethereum.on('chainChanged', (chainId) => {
+              if(chainId !== '0x89'){
+                  window.location.reload();
+              }
+          });
 
-            // get MATIC Balance
-            await web3.eth.getBalance(accounts[0]).then((res,er) => {
-                console.log(res);
-                console.log(er);
+          // get MATIC Balance
+          await web3.eth.getBalance(accounts[0]).then((res,er) => {
+              //console.log(res);
+              //console.log(er);
 
-                MATICBalance = web3.utils.fromWei(res, "ether");
-                console.log("MATIC Balance: "+MATICBalance);
-            });
+              MATICBalance = web3.utils.fromWei(res, "ether");
+              //console.log("MATIC Balance: "+MATICBalance);
+          });
 
-            // get DAI Balance
-            DAIcontract = new web3.eth.Contract(minABI, DAIContractAddress);
-            DAIBalance = await DAIcontract.methods.balanceOf(accounts[0]).call();
-            DAIBalance = web3.utils.fromWei(DAIBalance,"ether");
-            console.log("DAI Balance: "+DAIBalance);
+          // get DAI Balance
+          DAIcontract = new web3.eth.Contract(minABI, DAIContractAddress);
+          DAIBalance = await DAIcontract.methods.balanceOf(accounts[0]).call();
+          DAIBalance = web3.utils.fromWei(DAIBalance,"ether");
+          //console.log("DAI Balance: "+DAIBalance);
 
-
-
-            // select Token
-            selectToken();
-            
-        }
-        
+          // select Token
+          selectToken();
+          
+      }        
     } else {
         // If web3 is not available, give instructions to install MetaMask
-        document.getElementById('connect-error').innerHTML =
-            'Please install MetaMask or compatible wallet to connect to the Ethereum network.';
+        document.getElementById('connect-error').innerHTML = 'Please install MetaMask or compatible wallet to connect to the Ethereum network.';
     }
 }
 
 function getSafePolygonStats(){
   $.ajax({url:'/api/polygonscan.php', success: function(result){
-    console.log(result);
+    // Set current balances
+    document.getElementById('matic-safe-balance').innerHTML = "<h3>"+result.matic_balance+"</h3>";
+    document.getElementById('dai-safe-balance').innerHTML = "<h3>"+result.dai_balance+"</h3>";
+
+    // publish recent transactions
+    result.transactions.forEach(printTransactions);
   }});
 }
 
+function printTransactions(item, index){
+  // Only list the last 30 transactions
+  if(index <= 30){
+    // Determine token
+    icon_src = "";
+    if(item.token == 'matic'){
+      icon_src = "/img/matic_icon.png";
+    }
+    else if (item.token == 'dai'){
+      icon_src = "/img/dai_icon.png";
+    }
+    // Format Date
+    date = new Date(Number(item.timestamp) * 1000);
+    formatted_date = date.toLocaleDateString("en-US");
+
+    // Format From Address (shorten)
+    re = /^(.{5}).*(.{4})$/;
+    match = item.from.match(re);
+    formatted_addr = match[1]+"..."+match[2];
+        
+    // create row
+    row = `<div class="row justify-content-center">
+      <div class="col-4">
+        <small><img src="${icon_src}" width="25px"> ${item.amount}</small>
+      </div>
+      <div class="col-4">
+        <small><a href="https://polygonscan.com/address/${item.from}" target="_blank" class="link-light">${formatted_addr}</a></small>
+      </div>
+      <div class="col-4">
+        <small>${formatted_date}</small>
+      </div>
+    </div>`;
+    // Publish
+    $('#recent-transactions').append(row);
+  }
+}
 
 window.addEventListener('load', 
   function() { 
